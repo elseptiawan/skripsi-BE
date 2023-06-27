932f22591361fbf7bbfec84c58b7f5774d07b7a6
elseptiawan <septiawanelshandi@gmail.com>
2023-06-28 01:37:47 +0700

    import from excel

--- controllers/ImportController.js
+++ controllers/ImportController.js
@@ -24,29 +24,64 @@ exports.importexcel = async (req, res) => {
 
         const data = excelData.Sheet1
 
-        data.forEach((element) => { 
-            (async () => {
-             var {dataValues:category} = await Category.findOne({
+        data.forEach((element) => {
+            Category.findOrCreate({
                 where: {
-                    nama: {
-                        [Op.like]: element['Kategori']
-                    }
-                }});
-
-             const restoran = await Restoran.create({
-                kategori_id: category.kategori_id,
-                nama: element['Nama'],
-                kecamatan: element['Kecamatan'],
-                alamat: element['Alamat'],
-                no_sertifikat: element['Nomor Sertifikat'],
-                latitude: element['Latitude'],
-                longtitude: element['Longtitude']
-             })
-             })();
+                  nama: {
+                    [Op.like]: element["Kategori"],
+                  },
+                },
+                defaults: {
+                  nama: element["Kategori"],
+                },
+              }).then(([category]) => {
+                Restoran.create({
+                  kategori_id: category.kategori_id,
+                  nama: element["Nama"],
+                  kecamatan: element["Kecamatan"],
+                  alamat: element["Alamat"],
+                  no_sertifikat: element["Nomor Sertifikat"],
+                  latitude: element["Latitude"],
+                  longtitude: element["Longtitude"],
+                });
+              });
           });
+
+        // data.map((element) => { 
+        //     (async () => {
+        //     const [category, created] = await Category.findOrCreate({
+        //             where: {
+        //                 nama: {
+        //                     [Op.like]: element['Kategori']
+        //                 }
+        //             },
+        //             defaults: {
+        //                 nama: element['Kategori']
+        //               }
+        //         });
+
+        //      const restoran = await Restoran.create({
+        //         kategori_id: created ? created.kategori_id : category.kategori_id,
+        //         nama: element['Nama'],
+        //         kecamatan: element['Kecamatan'],
+        //         alamat: element['Alamat'],
+        //         no_sertifikat: element['Nomor Sertifikat'],
+        //         latitude: element['Latitude'],
+        //         longtitude: element['Longtitude']
+        //      });
+        //      })();
+        //   });
 
         res.status(200).json({message: "Success Import Data"});
     } catch (error) {
         res.status(400).json({success: 'false', message: error});
     }
+}
+
+const createCategory = async (nama) => {
+    const category = await Category.create({
+        nama: nama
+    });
+
+    return category;
 }
