bad08baa6b92e4d313b87aae16421a63e2bbf2b2
elseptiawan <septiawanelshandi@gmail.com>
2023-07-11 17:24:39 +0700

    validasi file import data

--- controllers/ImportController.js
+++ controllers/ImportController.js
@@ -8,6 +8,20 @@ var upload = multer({ dest: "file-excel/" });
 
 exports.importexcel = async (req, res) => {
     try{
+        if (!req.file){
+          return res.status(400).json({message: "Pilih File yang akan di-import"})
+        }
+
+        const file_extension = req.file.originalname.slice(
+          ((req.file.originalname.lastIndexOf('.') - 1) >>> 0) + 2
+        );
+
+        const array_of_allowed_files = ['xls', 'xlsx'];
+
+        if (!array_of_allowed_files.includes(file_extension)) {
+          return res.status(400).json({message: "Format file tidak didukung, Silahkan pilih file xls atau xlsx"});
+        }
+
         var filePath = "file-excel/" + req.file.filename;
 
         const excelData = excelToJson({
@@ -24,56 +38,33 @@ exports.importexcel = async (req, res) => {
 
         const data = excelData.Sheet1
 
-        data.forEach((element) => {
-          Category.findOrCreate({
+        const t = await sequelize.transaction();
+
+        data.forEach(async (element) => {
+          const category = await Category.findOne({
             where: {
               nama: {
                 [Op.like]: element["Kategori"],
               },
-            },
-            defaults: {
-              nama: element["Kategori"],
-            },
-          }).then(([category]) => {
-            Restoran.create({
-              kategori_id: category.kategori_id,
-              nama: element["Nama"],
-              kecamatan: element["Kecamatan"],
-              alamat: element["Alamat"],
-              no_sertifikat: element["Nomor Sertifikat"],
-              latitude: element["Latitude"],
-              longtitude: element["Longtitude"],
-            });
+            }
+          });
+
+          await Restoran.create({
+            kategori_id: category.kategori_id,
+            nama: element["Nama"],
+            kecamatan: element["Kecamatan"],
+            alamat: element["Alamat"],
+            no_sertifikat: element["Nomor Sertifikat"],
+            latitude: element["Latitude"],
+            longtitude: element["Longtitude"],
           });
         });
 
-        // data.map((element) => { 
-        //     (async () => {
-        //     const [category, created] = await Category.findOrCreate({
-        //             where: {
-        //                 nama: {
-        //                     [Op.like]: element['Kategori']
-        //                 }
-        //             },
-        //             defaults: {
-        //                 nama: element['Kategori']
-        //               }
-        //         });
-
-        //      const restoran = await Restoran.create({
-        //         kategori_id: created ? created.kategori_id : category.kategori_id,
-        //         nama: element['Nama'],
-        //         kecamatan: element['Kecamatan'],
-        //         alamat: element['Alamat'],
-        //         no_sertifikat: element['Nomor Sertifikat'],
-        //         latitude: element['Latitude'],
-        //         longtitude: element['Longtitude']
-        //      });
-        //      })();
-        //   });
+        await t.commit();
 
         res.status(200).json({message: "Success Import Data"});
     } catch (error) {
+        await t.rollback();
         res.status(400).json({success: 'false', message: error});
     }
 }
